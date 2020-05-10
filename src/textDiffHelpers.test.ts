49fb006ad85ab1aa096e4be02601cd49f66a95bf
musicalecho <musicalecho@gmail.com>
2020-05-10 11:04:09 +0300

    optimized test

--- src/textDiffHelpers.test.ts
+++ src/textDiffHelpers.test.ts
@@ -6,58 +6,35 @@ describe('textDiffHelpers:', () => {
 
   const testingData: Record<string, string>[] = [
     {
+      test: 'when added some characters',
       newText: 'Awesome text (not at all)',
       patch: '@@ -5,8 +5,21 @@\n ome text\n+ (not at all)\n',
     },
     {
+      test: 'when removed characters',
       newText: 'text',
       patch: '@@ -1,12 +1,4 @@\n-Awesome \n text\n',
     },
     {
+      test: 'when text changed',
       newText: 'Here could be your Lorem Ipsum',
       patch: '@@ -1,12 +1,30 @@\n-Aw\n+H\n e\n-s\n+re c\n o\n-m\n+uld b\n e \n-t\n+your Lor\n e\n-xt\n+m Ipsum\n',
     },
   ];
 
-  let i = 0;
-
-  afterEach(() => { i += 1; });
-
   describe('get patch:', () => {
-    beforeAll(() => { i = 0; });
-
-    it('when added some characters', () => {
-      const { newText, patch } = testingData[i];
-      expect(textToPatch(text, newText)).toEqual(patch);
-    });
-
-    it('when removed characters', () => {
-      const { newText, patch } = testingData[i];
-      expect(textToPatch(text, newText)).toEqual(patch);
-    });
-
-    it('when text changed', () => {
-      const { newText, patch } = testingData[i];
-      expect(textToPatch(text, newText)).toEqual(patch);
+    testingData.forEach(({ test, newText, patch }) => {
+      it(test, () => {
+        expect(textToPatch(text, newText)).toEqual(patch);
+      });
     });
   });
 
   describe('apply patch:', () => {
-    beforeAll(() => { i = 0; });
-
-    it('when added some characters', () => {
-      const { newText, patch } = testingData[i];
-      expect(patchToText(patch, text)).toEqual(newText);
-    });
-
-    it('when removed characters', () => {
-      const { newText, patch } = testingData[i];
-      expect(patchToText(patch, text)).toEqual(newText);
-    });
-
-    it('when text changed', () => {
-      const { newText, patch } = testingData[i];
-      expect(patchToText(patch, text)).toEqual(newText);
+    testingData.forEach(({ test, newText, patch }) => {
+      it(test, () => {
+        expect(patchToText(patch, text)).toEqual(newText);
+      });
     });
   });
 });
